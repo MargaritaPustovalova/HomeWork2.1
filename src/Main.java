adaee8f4c805f9b0d4883b6df035fbced5f26513
MargaritaPustovalova <belka_tannny@mail.ru>
2022-02-11 13:06:14 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+            int ticketCost = 1500;
+            int milesSum = ticketCost / 20;
+            System.out.println(milesSum);
+        }
+    }
